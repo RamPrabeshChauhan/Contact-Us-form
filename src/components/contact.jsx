bb33c89767614842015b541fec3f1039721324d1
RamPrabeshChauhan <88790768+RamPrabeshChauhan@users.noreply.github.com>
2022-01-24 23:10:08 +0530

    Contact Us Form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact()
 {
+    const [userFirstName, setUserFirstName] = useState("");
+    const [userLastName, setUserLastName] = useState("");
+    const [userPh, setUserPh] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userMessage, setUserMessage] = useState("");
+    
     const handleSubmit = (event) => {
         
-        const first = event.target.firstName.value;
-        const last = event.target.lastName.value;
-        const phone = event.target.phone.value;
-        const email = event.target.email.value;
-        const message = event.target.message.value;
+        // Method 2 to print user input data in console.
+        // const first = event.target.firstName.value;
+        // const last = event.target.lastName.value;
+        // const phone = event.target.phone.value;
+        // const email = event.target.email.value;
+        // const message = event.target.message.value;
 
-        console.log("First Name : " + first);
-        console.log("Last Name : " + last);
-        console.log("Phone : " + phone);
-        console.log("Gmail : " + email);
-        console.log("Message : " + message);
+        // console.log("First Name : " + first);
+        // console.log("Last Name : " + last);
+        // console.log("Phone : " + phone);
+        // console.log("Gmail : " + email);
+        // console.log("Message : " + message);
+
+        
+
+        // Method 3 to print user input data in console
+        console.log("First Name : " + userFirstName);
+        console.log("Last Name : " + userLastName);
+        console.log("Phone : " + userPh);
+        console.log("Gmail : " + userEmail);
+        console.log("Message : " + userMessage);
+
 
         event.preventDefault();
     }
@@ -26,28 +43,28 @@ function Contact()
                 <div className="person-name">
                     <div className="form-group gap">
                         <label htmlFor="firstName">First Name<br /></label>
-                        <input type="text" name="firstName" id="firstName" />
+                        <input type="text" name="firstName" id="firstName" onChange={(event) => setUserFirstName(event.target.value)}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="lastName">Last Name<br /></label>
-                        <input type="text" name="lastName" id="lastName" />
+                        <input type="text" name="lastName" id="lastName" onChange={(event) => setUserLastName(event.target.value)}/>
                     </div>
                 </div>
                 
                 <div className="person-contact">
                     <div className="form-group gap">
                         <label htmlFor="phone">Phone<br /></label>
-                        <input type="tel" name="phone" id="phone" />
+                        <input type="tel" name="phone" id="phone" onChange={(event) => setUserPh(event.target.value)}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email<br /></label>
-                        <input type="text" name="email" id="email" />
+                        <input type="text" name="email" id="email" onChange={(event) => setUserEmail(event.target.value)}/>
                     </div>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="message">Message<br /></label>
-                    <textarea type="text" name="message" id="message"  />
+                    <textarea type="text" name="message" id="message"  onChange={(event) => setUserMessage(event.target.value)}/>
                 </div>
 
                 <button type="submit" id="submit-button" >Submit</button>
